4f3cbcf99d820ab0a70a97d55a50af9fdb01e55d
曹瀚文 <hanwen.cao@xtransfer.cn>
2023-07-10 17:58:52 +0800

    feat: 补充初始化样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
-
 import App from "./app"
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
